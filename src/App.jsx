fb1f050229a1b3c78fc0a02b1cb99e3a3678de42
Roshan panda <roshanpanda666@gmail.com>
2023-05-18 21:47:28 +0530

    commiting

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div className='bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-gray-950 via-gray-950 to-violet-950 flex justify-center items-center'>
+      
       <img src="cover.png" alt="" className='h-96 rounded-tl-md'/>
+      hello meta
     </div>
   )
 }
